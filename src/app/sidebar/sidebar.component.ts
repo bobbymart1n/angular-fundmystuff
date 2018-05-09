8b4722d71596f2041ad6040ced59aa3d930315c7
Bobby Martin and jared lutz <bobby@>
2018-05-09 16:24:22 -0700

    add donation modal

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import { Info } from './../info.model';
 import { ItemService } from './../services/item.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,9 +13,11 @@ import { Location } from '@angular/common';
 })
 export class SidebarComponent implements OnInit {
   infoId: string;
-  donationClick = null
+  donationClick = null;
+  donations: number = 0;
   infoToDisplay;
-  constructor(private itemService: ItemService, private route: ActivatedRoute, private location: Location) { }
+  closeResult: string;
+  constructor(private itemService: ItemService, private route: ActivatedRoute, private location: Location, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
@@ -28,6 +31,27 @@ export class SidebarComponent implements OnInit {
     console.log(this.donationClick);
   }
 
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  submitDonation(newDonation){
+    this.donations += parseInt(newDonation);
+  }
   progress() {
 
   }
